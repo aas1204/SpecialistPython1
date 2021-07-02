24f4e6bf80a3980f82a022754af1900eeab69d9f
aas1204 <86655159+aas1204@users.noreply.github.com>
2021-07-02 11:22:53 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,13 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    n_str=str(number)
+    n_rev=n_str[::-1]
+    return n_str==n_rev
+
+
+print(palindrome(234532))
+print(palindrome(765467))
+print(palindrome(56765))
+print(palindrome(12345321))
+print(palindrome(9876789))
